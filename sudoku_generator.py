3ddd88f869f0feb824a1b4e18794005d7265cbf5
AHVG <augusto.h.v.guerner@gmail.com>
2023-06-24 19:12:31 -0300

    Nada demais

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -1,4 +1,4 @@
-
+import random
 
 def get_possible_numbers(sudoku, line, column):
     possible_numbers = [i for i in range(1, 10)]
@@ -12,7 +12,6 @@ def get_possible_numbers(sudoku, line, column):
             possible_numbers.remove(sudoku[line][i])
         if possible_numbers.count(sudoku[i][column]):
             possible_numbers.remove(sudoku[i][column])
-
 
     for x in range(s_r_x, e_r_x):
         for y in range(e_r_y, e_r_y):
@@ -30,13 +29,34 @@ def get_empty_spaces(sudoku):
     return positions[:]
 
 def generate_sudokus(n_sudokus):
-    sudoku = [[0 for __ in range(9)] for _ in range(9)]
-    states = [[get_possible_numbers(sudoku, line, column) for column in range(9)] for line in range(9)]
+    sudokus = []
+    states = [[[0 for i in range(9)] for j in range(9)]]
 
-    for _ in range(n_sudokus):
-        while len(get_empty_spaces(sudoku)) != 0:
-            
+    possibilities = [[get_possible_numbers(states[-1], l, c) for c in range(9)] for l in range(9)]
+    while True:
+        spaces = get_empty_spaces(states[-1])
+        if len(spaces) == 0:
+            if n_sudokus == len(sudokus):
+                break
+            else:
+                sudokus.append([[e for e in l] for l in states[-1]])
+                states.pop(-1)
+                break
 
+        line, column = spaces[0]
+        if len(possibilities[line][column]) == 0:
+            states.pop(-1)
+            continue
+        
+        number = random.choice(possibilities[line][column])
+        states.append([[e for e in l] for l in states[-1]])
+        states[-1][line][column] = number
+        possibilities[line][column].remove(number)
+
+    return states[-1]
 
 if __name__=="__main__":
-    generate_sudokus(1)
+    for line in generate_sudokus(1):
+        for e in line:
+            print(e, end="")
+        print()

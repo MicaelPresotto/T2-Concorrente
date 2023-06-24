3eac9c96882d2a09707efe51dcb3b18b4564c1ff
AHVG <augusto.h.v.guerner@gmail.com>
2023-06-24 15:26:45 -0300

    Estruturando os .pys

---
+++ graph.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot
+
+
+def graph():
+    pass
+
+
+
+if __name__ == "__main__":
+    graph()
---
+++ serial.py
@@ -0,0 +1,7 @@
+
+
+def serial():
+    pass
+
+if __name__ == "__main__":
+    serial()
